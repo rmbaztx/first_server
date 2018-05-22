eae0d53428b0f94aaf2604a1dacc3d99219bec4e
rmbaztx <rmbloodgood@mac.com>
2018-05-22 10:11:47 -0500

    UPdated

--- first_server_with_two.js
+++ first_server_with_two.js
@@ -6,7 +6,7 @@ var http = require("http");
 var PORT = 7000;
 
 // Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequest1(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
   response.end("Something good about you!! Path Hit: " + request.url);
@@ -14,7 +14,7 @@ function handleRequest(request, response) {
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest1);
 
 // Start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
@@ -23,13 +23,13 @@ server.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
 // Require/import the HTTP module
-var http = require("http");
+// var http = require("http");
 
 // Define a port to listen for incoming requests
 var PORT2 = 7500;
 
 // Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequest2(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
   response.end("Something bad about you!! Path Hit: " + request.url);
@@ -37,10 +37,10 @@ function handleRequest(request, response) {
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT2, function() {
+server2.listen(PORT2, function() {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT2);
